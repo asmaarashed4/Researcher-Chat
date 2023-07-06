9d456ebfc9e4e57b0c48bd8036e6fe0479d0116c
Asmaa Rashed <46477017+asmaarashed4@users.noreply.github.com>
2023-07-07 02:58:11 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,3 +1,15 @@
+!pip install wget #python package to download files from internet
+#langchain a software development framework designed to simplify the creation of applications using large language models
+!pip install langchain
+!pip install faiss-cpu
+#tiktoken is a fast BPE tokeniser for use with OpenAI's models
+!pip install tiktoken
+#PyPDF2 is a free and open-source pure-python PDF library capable of splitting, merging, cropping, and transforming the pages of PDF files
+!pip install PyPDF2
+#we will use wget to download our pdf file
+!pip install wget
+# to work with openAI
+!pip install openai
 import streamlit as st # to import libarary
 import math
 import wget
@@ -5,7 +17,6 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
-import wget
 
 # ---------------- to creates the title ------------------
 st.title("Researcher Chat Assistant")
